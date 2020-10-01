3e07b2ceccf244e52f6f35725416e9aae8bac26f
A L <AL@email.com>
2020-10-01 16:36:41 -0400

    Pass pipeline context

--- src/providers/AWS.groovy
+++ src/providers/AWS.groovy
@@ -3,11 +3,14 @@ package providers
 import common.DNS.AWS.AWS_DNS
 
 class AWS {
-    AWS() {
+    def context
+
+    AWS(pipelineContext) {
         println 'AWS constructor'
+        this.context = pipelineContext
     }
 
     def AWS_DNS DNS() {
-        return new AWS_DNS()
+        return new AWS_DNS(this.context)
     }
 }
